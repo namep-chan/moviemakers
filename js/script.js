6196d9374d083fdf3a4e1e4e954a104737b5a3cb
namep-chan <150093206+namep-chan@users.noreply.github.com>
2023-12-05 17:50:42 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,68 @@
+/*ヘッダー*/
+
+$('a[href^="#"]').click(function () {
+    const speed = 600;
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? "html" : href);
+    let position = target.offset().top;
+    $("body,html").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
+
+
+
+  /*ギャラリー*/
+
+  $('.slider').slick({
+		arrows: false,//左右の矢印はなし
+		autoplay: true,//自動的に動き出すか。初期値はfalse。
+		autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
+		speed: 6900,//スライドのスピード。初期値は300。
+		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+		pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
+		pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
+		cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
+		slidesToShow: 6,//スライドを画面に4枚見せる
+		slidesToScroll: 1,//1回のスライドで動かす要素数
+		responsive: [
+			{
+			breakpoint: 769,//モニターの横幅が769px以下の見せ方
+			settings: {
+				slidesToShow: 4,//スライドを画面に2枚見せる
+			}
+		},
+		{
+			breakpoint: 426,//モニターの横幅が426px以下の見せ方
+			settings: {
+				slidesToShow: 2,//スライドを画面に1.5枚見せる
+			}
+		}
+	]
+	});
+
+
+
+/*qa*/
+
+  //アコーディオンをクリックした時の動作
+$('.qa-title').on('click', function() {//タイトル要素をクリックしたら
+	var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
+	$(findElm).slideToggle();//アコーディオンの上下動作
+    
+	if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
+		$(this).removeClass('close');//クラス名を除去し
+	}else{//それ以外は
+		$(this).addClass('close');//クラス名closeを付与
+	}
+});
+
+//ページが読み込まれた際にopenクラスをつけ、openがついていたら開く動作※不必要なら下記全て削除
+$(window).on('load', function(){
+	$('.accordion-area li:first-of-type section').addClass("open"); //accordion-areaのはじめのliにあるsectionにopenクラスを追加
+	$(".open").each(function(index, element){	//openクラスを取得
+		var Title =$(element).children('.qa-title');	//openクラスの子要素のtitleクラスを取得
+		$(Title).addClass('close');				//タイトルにクラス名closeを付与し
+		var Box =$(element).children('.box');	//openクラスの子要素boxクラスを取得
+		$(Box).slideDown(500);					//アコーディオンを開く
+	});
+});
